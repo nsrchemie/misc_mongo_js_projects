e60690190ae5620e98d0d432957ed2a2e1d93f40
nsrchemie <nsrchemie@gmail.com>
2017-04-21 18:42:16 -0400

    added invoice and customer routes

--- invoicr/app.js
+++ invoicr/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const customers = require('./routes/customers');
+const invoices = require('./routes/invoices');
 mongoose.connect('mongodb://localhost/invoicr');
 const db = mongoose.connection;
 
@@ -12,6 +13,9 @@ app.use(bodyParser.json());
 app.get('/', (req,res) => {
 	res.send('Please use /api/customers or /api/invoices');
 });
+
+app.use('/api/customers', customers);
+app.use('/api/invoices', invoices);
 
 app.listen(3000);
 console.log('Listening on port 3000');
